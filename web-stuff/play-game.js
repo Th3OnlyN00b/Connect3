14c5b0148d28cc9e9fcc76015a3e112c425328af
Shirley Xu <shirleyxu@umass.edu>
2018-10-14 03:52:14 -0400

    Fixxited it

--- web-stuff/play-game.js
+++ web-stuff/play-game.js
@@ -1,4 +1,4 @@
-var connection = new WebSocket('ws://1b34ccd2.ngrok.io:9995');
+var connection = new WebSocket('ws://1b34ccd2.ngrok.io');
 var player = "";
 var availableSpaces = "01234";
 var victory = -1;
@@ -9,22 +9,18 @@ var connected = false;
 var p1 = 2;
 var p2 = 1;
 var possibleTypes = ["human", "random", "aw", "minimax", "pruned-mm"];
-var formal_names = {"human": "Human", "random": "Random with Auto", "aw": "Automatic", "minimax": "Minmax", "pruned-mm": "Pruned Minmax"};
+var formal_names = {"human": "Human", "random": "Random with Auto", "aw": "Automatic", "minimax": "Minmax", "pruned-mm": "Pruned Minmax", "hw": "Hardware"};
 var waiting = (p1 == "human" && p2 == "human");
+var playerTypeList = parseURL();
+var p1Type = playerTypeList.length>0?playerTypeList[0]:null;
+var p2Type = playerTypeList.length>1?playerTypeList[1]:null;
+var joining = playerTypeList.length>2?playerTypeList[2]:null;
 
 // Replace player placeholders with player names
-$(document).ready(function() {
-  var playerTypeList = parseURL();
-  var p1Type = "";
-  var p2Type = "";
-  var joining = "";
+window.onload = function (){
 
   // If everything is correct, set the players and start the game
   if(playerTypeList != null){
-    p1Type = playerTypeList[0];
-    p2Type = playerTypeList[1];
-    joining = playerTypeList[2];
-
     document.getElementById("player1Tag").innerHTML = formal_names[p1Type];
     document.getElementById("player2Tag").innerHTML = formal_names[p2Type];
   } 
@@ -33,7 +29,7 @@ $(document).ready(function() {
   else {
     victory = 0;
   }
-})
+}
 
 // Confirm connection
 connection.onopen = function (e) {
@@ -62,20 +58,20 @@ connection.onmessage = function (e) {
 
     if(joining && player == p1){
       alert("Error: There is no game to join!")
+      console.log("Error: no game to join");
     }
 
     var playerLetter = player==1?"A":"B";
     connection.send(playerLetter + "human");
     console.log("Player Letter has been sent: " + playerLetter + "human");
     connected = true;
-
-    document.getElementById("subheader").innerHTML = "Welcome, player " + player + "!"
   }
 
   // Update the grid if there is a command received to do so, and toggle the player turn
   else if(first_char == 'd'){
     colorSpaces(string_data.slice(1, string_data.length));
     turn = turn==1?2:1;
+    document.getElementById("subheader").innerHTML = "Player " + turn + "'s turn to play!";
   }
 
   // Update the availability of the columns
@@ -121,17 +117,26 @@ function sendClick(cell){
 
   // The game cannot start if there is no connection to the server!
   if(connected == false){
-    alert("Error: Problem setting up the game. Check your URL or your connection.");
+    alert("Error: Problem interacting with the game. Check your URL or your connection.");
+    console.log("Error: no connection to server");
   }
 
   // Player 1, if a human, must wait until player 2 appears
   else if(waiting){
     alert("Waiting for the second player to join the server...");
+    console.log("Waiting for player 2...");
+  }
+
+  // You cannot play when it is not your turn
+  else if(String(turn) != player){
+    alert("It's not your turn!");
+    console.log("Attempt to play - not on turn");
   }
 
   // Makes sure space isn't already taken
   else if(document.getElementById(cell).style.backgroundColor != ""){
     showInvalidMoveModal();
+    console.log("Attempt to play - space already taken");
   } 
 
   // Continue, as long as the game isn't over
@@ -156,18 +161,20 @@ function parseURL(){
   // Make sure that there is the right number and type of queries
   if(queries.length != 3 || queries[0].slice(0,2) != "p1" || queries[1].slice(0,2) != "p2"){
     alert("There seems to be something wrong with the URL. Please make sure that both players have been selected on a previous page.");
+    console.log("Error: improper URL");
     return;
   }
 
   // Make sure that both players are valid types
   if(!possibleTypes.includes(queries[0].slice(3)) || !possibleTypes.includes(queries[1].slice(3))){
     alert("A player seems to be of an invalid type. Please make sure that both players have been selected on a previous page.");
+    console.log("Error: bad URL type");
     return;
   }
   else{
     var query1 = queries[0].slice(3);
     var query2 = queries[1].slice(3);
-    var query2 = queries[2].slice(5);
+    var query3 = queries[2].slice(5);
     if(query2.charAt(query2.length - 1) == "#"){
       query2 = query2.slice(0, query2.length - 1);
     }
